82b086ab272a6d2efe42e924099972db9dd4c9f6
sujonbiswas212 <wwwsujonbiswas212@gmail.com>
2025-05-06 02:47:25 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["*"],
+  theme: {
+    colors:{
+      gray1:'#f4f4f5',
+      graay:'#FAFAFA',
+      red1:'#e7000b',
+      green1:'#00a63e',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
